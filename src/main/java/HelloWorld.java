58fcf540db0f29691c5d8db80b265d5ea5a6cbbd
nieyo <jonasbucher95@gmail.com>
2025-01-28 14:53:39 +0100

    add HelloWorld

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,49 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Java is cool!");
+
+        int x;
+        x = 1;
+
+        System.out.println(x);
+
+
+        int a = 17;
+        int b = 23;
+
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(b / a);
+
+        a = 44;
+        b = 12;
+
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+
+        float c = 2.3F;
+        float d = 12.9F;
+
+        System.out.println(c + d);
+        System.out.println(c - d);
+        System.out.println(c * d);
+        System.out.println(d / c);
+
+        c = 7.0F;
+        d = 2.1F;
+
+        System.out.println(c > d);
+        System.out.println(c < d);
+        System.out.println(c == d);
+        System.out.println(c != d);
+
+    }
+}
