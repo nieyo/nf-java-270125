bd428ea9573e87cc5daf87599825eb5e4fffcd9c
nieyo <jonasbucher95@gmail.com>
2025-01-29 11:36:13 +0100

    add ArrayAndLoop

---
+++ src/main/java/ArrayAndForLoop.java
@@ -0,0 +1,38 @@
+public class ArrayAndForLoop {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+        int[] numbers2 = {20,19,18,17,16,15,14,13,12,11};
+        int[] numbersSum = new int[10];
+
+        for(int number : numbers) {
+            System.out.println(number);
+        }
+        System.out.println("-----");
+
+        int sum = 0;
+        int highestNumber = 0;
+
+        // find shorter array
+        // numbers.length < numbers2.length ? int index = numbers.length : int index = numbers2.length
+
+        for (int i = 0; i < numbers.length; i++) {
+            // sum
+            sum += numbers[i];
+            // highest number
+            if(numbers[i] > highestNumber) {
+                highestNumber = numbers[i];
+            }
+            // addition of arrays
+            numbersSum[i] = numbers[i] + numbers2[i];
+        }
+
+
+        System.out.println("sum: " + sum);
+        System.out.println("highest number: " + highestNumber);
+        System.out.println("-----");
+
+        for(int number : numbersSum) {
+            System.out.println(number);
+        }
+    }
+}
