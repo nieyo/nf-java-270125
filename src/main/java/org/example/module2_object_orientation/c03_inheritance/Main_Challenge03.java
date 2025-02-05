d8b29522c93776fa2b988d4fc0718a8a4eae1aa7
nieyo <jonasbucher95@gmail.com>
2025-02-05 11:20:35 +0100

    Alter: minor changes in console output

--- src/main/java/org/example/module2_object_orientation/c03_inheritance/Main_Challenge03.java
+++ src/main/java/org/example/module2_object_orientation/c03_inheritance/Main_Challenge03.java
@@ -4,11 +4,13 @@ public class Main_Challenge03 {
     public static void main(String[] args) {
         Vehicle v1 = new Vehicle("Piaggio", "Ape Calessino", "1956");
         Car c1 = new Car("BMW", "MINI Cooper Hatchback", "2024", 3);
-        Motorcycle m1 = new Motorcycle("Harley-Davidson", "Road Glide", "2024", "Touring");
+        Motorcycle m1 = new Motorcycle("Harley-Davidson", "Road Glide", "2024", "Touring Motorcycle");
 
         v1.printInformation();
-        System.out.println(v1.toString());
         c1.printInformation();
+        m1.printInformation();
+
         System.out.println(m1.getType());
+
     }
 }
