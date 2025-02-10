d4563d2425aae1cb3571f512f059afe4ef9cb9b5
nieyo <jonasbucher95@gmail.com>
2025-02-10 14:35:14 +0100

    Add: boiler code for class ProductRepo
    - add getters, setters, equals, hashcode, toString

--- src/main/java/org/example/module2_object_orientation/c09_recap_project/ProductRepo.java
+++ src/main/java/org/example/module2_object_orientation/c09_recap_project/ProductRepo.java
@@ -1,7 +1,41 @@
 package org.example.module2_object_orientation.c09_recap_project;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ProductRepo {
     private ArrayList<Product> repository;
+
+    // add methods to add, remove, and get products (single product and all products)
+
+    public ProductRepo(ArrayList<Product> repository) {
+        this.repository = repository;
+    }
+
+    public ArrayList<Product> getRepository() {
+        return repository;
+    }
+
+    public void setRepository(ArrayList<Product> repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductRepo that = (ProductRepo) o;
+        return Objects.equals(repository, that.repository);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(repository);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductRepo{" +
+                "repository=" + repository +
+                '}';
+    }
 }
