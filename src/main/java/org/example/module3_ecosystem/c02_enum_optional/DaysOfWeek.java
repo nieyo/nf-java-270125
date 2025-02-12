f692e82ccae8110b74a02a5ca61633c7e739c820
nieyo <jonasbucher95@gmail.com>
2025-02-12 12:25:15 +0100

    refactor: enum DaysOfWeek
    - Add attribute friendlyName and constructor to enum DaysOfWeek
    - Alter method getDayType to return friendlyName
    - Add comment

--- src/main/java/org/example/module3_ecosystem/c02_enum_optional/DaysOfWeek.java
+++ src/main/java/org/example/module3_ecosystem/c02_enum_optional/DaysOfWeek.java
@@ -1,15 +1,22 @@
 package org.example.module3_ecosystem.c02_enum_optional;
 
 public enum DaysOfWeek {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY;
+    MONDAY("Monday"),
+    TUESDAY("Tuesday"),
+    WEDNESDAY("Wednesday"),
+    THURSDAY("Thursday"),
+    FRIDAY("Friday"),
+    SATURDAY("Saturday"),
+    SUNDAY("Sunday");
 
+    private final String friendlyName;
+
+    DaysOfWeek(String friendlyName){
+        this.friendlyName = friendlyName;
+    }
+
+    // ordinal() returns position of declaration in the enum, starting from zero
     public String getDayType() {
-        return this.ordinal() <= 4 ? this.name() : "WEEKEND";
+        return this.ordinal() <= 4 ? this.friendlyName : "Weekend";
     }
 }
