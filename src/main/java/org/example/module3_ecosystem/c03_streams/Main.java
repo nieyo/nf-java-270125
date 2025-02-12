9244156f2aab6dd1aabe8576b3a92227bda936f5
nieyo <jonasbucher95@gmail.com>
2025-02-12 16:09:27 +0100

    Add: package c03_streams

---
+++ src/main/java/org/example/module3_ecosystem/c03_streams/Main.java
@@ -0,0 +1,65 @@
+package org.example.module3_ecosystem.c03_streams;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        // generate list of numbers 1 to 10
+        List<Integer> numberList = generateNumberList(10, 1);
+
+        // output even numbers
+        numberList.stream()
+                .filter(number -> number % 2 == 0)
+                .forEach(System.out::println);
+
+        // double the numbers
+        numberList.stream()
+                .map(number -> number * 2)
+                .forEach(System.out::println);
+        List<Integer> doubledNumbers = numberList.stream()
+                .map(number -> number * 2)
+                .toList();
+
+        // sort ascending, sorted() sortiert default by value
+        // numberList.stream().sorted(Comparator.comparingInt(Integer::intValue)).forEach(System.out::println);
+        numberList.stream()
+                .sorted()
+                .forEach(System.out::println);
+
+        // sum, 0 is initial value
+        System.out.println(numberList.stream()
+                .reduce(0, Integer::sum));
+
+        // BONUS
+
+        List<Student> students = Files.lines(Path.of("students.csv"))
+                .skip(1)
+                .map(line -> line.split(","))
+                .filter(parts -> parts.length == 4)
+                .map(parts -> new Student(
+                        Integer.parseInt(parts[0]),
+                        parts[1],
+                        parts[2],
+                        Integer.parseInt(parts[3]))
+                )
+                .toList();
+
+        System.out.println(students);
+
+    }
+
+    public static List<Integer> generateNumberList(int size, int startValue) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            numbers.add(startValue + i);
+        }
+        return numbers;
+    }
+}
---
+++ src/main/java/org/example/module3_ecosystem/c03_streams/Student.java
@@ -0,0 +1,28 @@
+package org.example.module3_ecosystem.c03_streams;
+
+import java.util.List;
+
+public class Student {
+    private int id;
+    private String name;
+    private String postalCode;
+    private int age;
+
+    // Constructor
+    public Student(int id, String name, String postalCode, int age) {
+        this.id = id;
+        this.name = name;
+        this.postalCode = postalCode;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
