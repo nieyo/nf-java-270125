84c846e53c37c4865a0f15e15dae5cacd6e2fa10
nieyo <jonasbucher95@gmail.com>
2025-02-14 11:17:32 +0100

    Add: more Annotations

--- src/main/java/org/example/module3_ecosystem/c05_lombok/Main.java
+++ src/main/java/org/example/module3_ecosystem/c05_lombok/Main.java
@@ -6,33 +6,49 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        Student s1 = Student.builder()
-                .id("1")
-                .name("Max")
+        Student s1 = new Student("1", "Max", "address 1", 3.4f);
+        Student s2 = new Student("2", "Maxi", "address 2", 2.8f);
+
+        Student s3 = Student.builder()
+                .id("3")
+                .name("Maxim")
                 .grade(2.0f)
-                .address("address")
+                .address("address 3")
                 .build();
 
-        Student s2 = Student.builder()
-                .id("2")
-                .name("Maxi")
+        Student s4 = Student.builder()
+                .id("4")
+                .name("Maxima")
                 .grade(1.8f)
-                .address("address")
+                .address("address 4")
                 .build();
 
-        Teacher t1 = Teacher.builder()
-                .id("1")
-                .name("Alice")
+        Teacher t1 = new Teacher("1", "Alice", "english");
+        Teacher t2 = Teacher.builder()
+                .id("2")
+                .name("Bob")
                 .subject("maths")
                 .build();
+        Teacher t3 = t2.withId("3").withName("Carol");
 
-        Course c1 = Course.builder()
+
+        Course c1 = new Course("1", "course 1", t1, Arrays.asList(s1, s2, s3, s4));
+        Course c2 = Course.builder()
                 .students(new ArrayList<>(Arrays.asList(s1, s2)))
-                .teacher(t1)
-                .id("1")
-                .name("awesome course")
+                .teacher(t2)
+                .id("2")
+                .name("course 2")
+                .build();
+        Course c3 = Course.builder()
+                .students(new ArrayList<>(Arrays.asList(s3, s4)))
+                .teacher(t3)
+                .id("3")
+                .name("course 3")
                 .build();
 
+
         System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
     }
 }
--- src/main/java/org/example/module3_ecosystem/c05_lombok/Teacher.java
+++ src/main/java/org/example/module3_ecosystem/c05_lombok/Teacher.java
@@ -1,8 +1,10 @@
 package org.example.module3_ecosystem.c05_lombok;
 
 import lombok.Builder;
+import lombok.With;
 
 @Builder
+@With
 public record Teacher(
         String id,
         String name,
